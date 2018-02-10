90691ced6c2f0ed4cb97e23296f23b30c30495a3
Abhinav KM <abhinavkm184@gmail.com>
2018-02-10 13:41:17 +0530

    something changed

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-var http = require('http');
+const http = require('http');
+const url = require('url');
+const mysql = require('mysql');
+
+const conn = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"root"
+});
+
+conn.connect(function(err){
+    if (err){
+        throw err;
+    }
+    console.log('Connected');
+})
 
 http.createServer(function(req,res){
     res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end("Hello world");
+    res.end("Hello");
+
+    var q = url.parse(req.url,true);
+    console.log(q);
+    // conn.query(q., function(err,result){
+    //     if (err) throw err;
+    //     console.log("Result : " + result);
+    // });
+    
+    
+
 }).listen(8090);
+
+console.log("Server Started at 127.0.0.1:8090.....");
