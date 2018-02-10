ed7f82ca685b38110689a604ba1686ce3653f85b
Vishnu-M <vishnumurali7000@gmail.com>
2018-02-10 08:00:12 +0530

    Used Express library

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var http = require('http');
+var express = require('express')
+var app = express()
 
-http.createServer(function(req,res){
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end("Hello world");
-}).listen(8090);
+app.get('/', function(req, res){
+    res.send('Hello World!!')
+})
+
+app.listen(8090)
